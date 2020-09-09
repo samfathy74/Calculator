bd57300d809ee0a442ea4742508ac1849c834024
samfathy74 <salim.fat7y74@gmail.com>
2020-09-09 15:59:08 +0200

    .

---
+++ Form1.Designer.cs
@@ -0,0 +1,365 @@
+﻿namespace Calcu
+{
+    partial class Calculate
+    {
+        private System.ComponentModel.IContainer components = null;
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculate));
+            this.TextBoxScreen = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.buttonDot = new System.Windows.Forms.Button();
+            this.buttonChangeSign = new System.Windows.Forms.Button();
+            this.buttonEqual = new System.Windows.Forms.Button();
+            this.buttonSqurt = new System.Windows.Forms.Button();
+            this.buttonPresent = new System.Windows.Forms.Button();
+            this.buttonInverse = new System.Windows.Forms.Button();
+            this.buttonPlus = new System.Windows.Forms.Button();
+            this.buttonDiv = new System.Windows.Forms.Button();
+            this.buttonMult = new System.Windows.Forms.Button();
+            this.buttonMiuns = new System.Windows.Forms.Button();
+            this.buttonMplus = new System.Windows.Forms.Button();
+            this.buttonMC = new System.Windows.Forms.Button();
+            this.buttonMR = new System.Windows.Forms.Button();
+            this.buttonMS = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonBackSpace = new System.Windows.Forms.Button();
+            this.LblDeveloper = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextBoxScreen
+            // 
+            this.TextBoxScreen.BackColor = System.Drawing.Color.Gainsboro;
+            this.TextBoxScreen.Cursor = System.Windows.Forms.Cursors.IBeam;
+            resources.ApplyResources(this.TextBoxScreen, "TextBoxScreen");
+            this.TextBoxScreen.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.TextBoxScreen.Name = "TextBoxScreen";
+            this.TextBoxScreen.ReadOnly = true;
+            this.TextBoxScreen.ShortcutsEnabled = false;
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.UpdateOPeration);
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.UpdateOPeration);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.UpdateOPeration);
+            // 
+            // button4
+            // 
+            this.button4.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.UpdateOPeration);
+            // 
+            // button5
+            // 
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.UpdateOPeration);
+            // 
+            // button6
+            // 
+            this.button6.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.UpdateOPeration);
+            // 
+            // button7
+            // 
+            this.button7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.Aqua;
+            this.button7.FlatAppearance.BorderSize = 5;
+            this.button7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkCyan;
+            this.button7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Bisque;
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.UpdateOPeration);
+            // 
+            // button8
+            // 
+            this.button8.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.UpdateOPeration);
+            // 
+            // button9
+            // 
+            this.button9.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.Name = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.UpdateOPeration);
+            // 
+            // button0
+            // 
+            this.button0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button0.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.button0, "button0");
+            this.button0.Name = "button0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.UpdateOPeration);
+            // 
+            // buttonDot
+            // 
+            resources.ApplyResources(this.buttonDot, "buttonDot");
+            this.buttonDot.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonDot.Name = "buttonDot";
+            this.buttonDot.UseVisualStyleBackColor = true;
+            this.buttonDot.Click += new System.EventHandler(this.UpdateOPeration);
+            // 
+            // buttonChangeSign
+            // 
+            this.buttonChangeSign.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonChangeSign, "buttonChangeSign");
+            this.buttonChangeSign.Name = "buttonChangeSign";
+            this.buttonChangeSign.UseVisualStyleBackColor = true;
+            this.buttonChangeSign.Click += new System.EventHandler(this.buttonChangeSign_Click);
+            // 
+            // buttonEqual
+            // 
+            this.buttonEqual.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonEqual, "buttonEqual");
+            this.buttonEqual.Name = "buttonEqual";
+            this.buttonEqual.UseVisualStyleBackColor = true;
+            this.buttonEqual.Click += new System.EventHandler(this.buttonEqual_Click);
+            // 
+            // buttonSqurt
+            // 
+            this.buttonSqurt.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonSqurt, "buttonSqurt");
+            this.buttonSqurt.Name = "buttonSqurt";
+            this.buttonSqurt.UseVisualStyleBackColor = true;
+            this.buttonSqurt.Click += new System.EventHandler(this.buttonSqurt_Click);
+            // 
+            // buttonPresent
+            // 
+            this.buttonPresent.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonPresent, "buttonPresent");
+            this.buttonPresent.Name = "buttonPresent";
+            this.buttonPresent.UseVisualStyleBackColor = true;
+            this.buttonPresent.Click += new System.EventHandler(this.buttonPresent_Click);
+            // 
+            // buttonInverse
+            // 
+            this.buttonInverse.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonInverse, "buttonInverse");
+            this.buttonInverse.Name = "buttonInverse";
+            this.buttonInverse.UseVisualStyleBackColor = true;
+            this.buttonInverse.Click += new System.EventHandler(this.buttonInverse_Click);
+            // 
+            // buttonPlus
+            // 
+            this.buttonPlus.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonPlus, "buttonPlus");
+            this.buttonPlus.Name = "buttonPlus";
+            this.buttonPlus.UseVisualStyleBackColor = true;
+            this.buttonPlus.Click += new System.EventHandler(this.OperatorMathmatic);
+            // 
+            // buttonDiv
+            // 
+            this.buttonDiv.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonDiv, "buttonDiv");
+            this.buttonDiv.Name = "buttonDiv";
+            this.buttonDiv.UseVisualStyleBackColor = true;
+            this.buttonDiv.Click += new System.EventHandler(this.OperatorMathmatic);
+            // 
+            // buttonMult
+            // 
+            this.buttonMult.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonMult, "buttonMult");
+            this.buttonMult.Name = "buttonMult";
+            this.buttonMult.UseVisualStyleBackColor = true;
+            this.buttonMult.Click += new System.EventHandler(this.OperatorMathmatic);
+            // 
+            // buttonMiuns
+            // 
+            this.buttonMiuns.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonMiuns, "buttonMiuns");
+            this.buttonMiuns.Name = "buttonMiuns";
+            this.buttonMiuns.UseVisualStyleBackColor = true;
+            this.buttonMiuns.Click += new System.EventHandler(this.OperatorMathmatic);
+            // 
+            // buttonMplus
+            // 
+            this.buttonMplus.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonMplus, "buttonMplus");
+            this.buttonMplus.Name = "buttonMplus";
+            this.buttonMplus.UseVisualStyleBackColor = true;
+            this.buttonMplus.Click += new System.EventHandler(this.buttonMplus_Click);
+            // 
+            // buttonMC
+            // 
+            this.buttonMC.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonMC, "buttonMC");
+            this.buttonMC.Name = "buttonMC";
+            this.buttonMC.UseVisualStyleBackColor = true;
+            this.buttonMC.Click += new System.EventHandler(this.buttonMC_Click);
+            // 
+            // buttonMR
+            // 
+            this.buttonMR.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonMR, "buttonMR");
+            this.buttonMR.Name = "buttonMR";
+            this.buttonMR.UseVisualStyleBackColor = true;
+            this.buttonMR.Click += new System.EventHandler(this.buttonMR_Click);
+            // 
+            // buttonMS
+            // 
+            this.buttonMS.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.buttonMS, "buttonMS");
+            this.buttonMS.Name = "buttonMS";
+            this.buttonMS.UseVisualStyleBackColor = true;
+            this.buttonMS.Click += new System.EventHandler(this.buttonMS_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.buttonClear, "buttonClear");
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonBackSpace
+            // 
+            this.buttonBackSpace.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.buttonBackSpace.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.buttonBackSpace, "buttonBackSpace");
+            this.buttonBackSpace.Name = "buttonBackSpace";
+            this.buttonBackSpace.UseVisualStyleBackColor = true;
+            this.buttonBackSpace.Click += new System.EventHandler(this.buttonBackSpace_Click);
+            // 
+            // LblDeveloper
+            // 
+            resources.ApplyResources(this.LblDeveloper, "LblDeveloper");
+            this.LblDeveloper.ForeColor = System.Drawing.Color.Silver;
+            this.LblDeveloper.Name = "LblDeveloper";
+            this.LblDeveloper.MouseLeave += new System.EventHandler(this.LblDeveloper_MouseLeave);
+            // 
+            // Calculate
+            // 
+            this.AcceptButton = this.buttonEqual;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.CancelButton = this.buttonBackSpace;
+            this.Controls.Add(this.LblDeveloper);
+            this.Controls.Add(this.TextBoxScreen);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonBackSpace);
+            this.Controls.Add(this.buttonMplus);
+            this.Controls.Add(this.buttonMC);
+            this.Controls.Add(this.buttonMR);
+            this.Controls.Add(this.buttonMS);
+            this.Controls.Add(this.buttonPlus);
+            this.Controls.Add(this.buttonDiv);
+            this.Controls.Add(this.buttonMult);
+            this.Controls.Add(this.buttonMiuns);
+            this.Controls.Add(this.buttonEqual);
+            this.Controls.Add(this.buttonSqurt);
+            this.Controls.Add(this.buttonPresent);
+            this.Controls.Add(this.buttonInverse);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.buttonDot);
+            this.Controls.Add(this.buttonChangeSign);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.KeyPreview = true;
+            this.Name = "Calculate";
+            this.MouseHover += new System.EventHandler(this.Calculate_MouseHover);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBoxScreen;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button buttonDot;
+        private System.Windows.Forms.Button buttonChangeSign;
+        private System.Windows.Forms.Button buttonEqual;
+        private System.Windows.Forms.Button buttonSqurt;
+        private System.Windows.Forms.Button buttonPresent;
+        private System.Windows.Forms.Button buttonInverse;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonDiv;
+        private System.Windows.Forms.Button buttonMult;
+        private System.Windows.Forms.Button buttonMiuns;
+        private System.Windows.Forms.Button buttonMplus;
+        private System.Windows.Forms.Button buttonMC;
+        private System.Windows.Forms.Button buttonMR;
+        private System.Windows.Forms.Button buttonMS;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonBackSpace;
+        private System.Windows.Forms.Label LblDeveloper;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Calcu
+{
+    public partial class Calculate : Form
+    {
+        bool isNewEntity = false;
+        bool isRepeatLastOperation = false;   //too boolen var to block repete number and operation , and allow to clean screen to new var;
+        double EndResult = 0, NumbTwo = 0, Memory = 0;  //number result and next number input and memoryStore;
+        char ChPreviousOperator; //signs in calcu;
+        //
+        public Calculate()
+        {
+            InitializeComponent();
+            ActiveControl = TextBoxScreen;
+        }
+ //=======================================================================group1===================================================//
+        // method of numbers 1,2,3,4,5,6,7,8,9,0,. ;
+        private void UpdateOPeration(object sender, EventArgs e)
+        {
+            if (isNewEntity)
+            {
+                TextBoxScreen.Text = "0";
+                isNewEntity = false;
+            }
+            if (isRepeatLastOperation)
+            {
+                ChPreviousOperator = '\0';
+                EndResult = 0;
+            }
+            //use single line if statement.
+
+            if (!(TextBoxScreen.Text == "0" && (Button)sender == button0) && !(((Button)sender) == buttonDot && TextBoxScreen.Text.Contains(".")))
+            {
+                TextBoxScreen.Text = (TextBoxScreen.Text == "0" && ((Button)sender) == buttonDot) ? "0." : ((TextBoxScreen.Text == "0") ? ((Button)sender).Text : TextBoxScreen.Text + ((Button)sender).Text);
+            }
+        }
+//=======================================================================group2===================================================//
+        //method to perform operators +,-,*,/ ;
+        void Operators(double FirstNumber, char Sign, double SecoundNumber)
+        {
+            switch (ChPreviousOperator)
+            {
+                case '+':
+                    TextBoxScreen.Text = (EndResult = (FirstNumber + SecoundNumber)).ToString();
+                    break;
+                case '-':
+                    TextBoxScreen.Text = (EndResult = (FirstNumber - SecoundNumber)).ToString();
+                    break;
+                case '*':
+                    TextBoxScreen.Text = (EndResult = (FirstNumber * SecoundNumber)).ToString();
+                    break;
+                case '/':
+                        TextBoxScreen.Text = (EndResult = (FirstNumber / SecoundNumber)).ToString();
+                    break;
+            }
+        }
+
+        //method for perfrm mathmatic operation through operators method;
+        private void OperatorMathmatic(object sender, EventArgs e)
+        {
+          if (TextBoxScreen.Text.Length==0)
+            {
+                    TextBoxScreen.Text = null;
+            }
+            else
+            {
+            if (ChPreviousOperator == '\0')
+                {
+                    ChPreviousOperator = ((Button)sender).Text[0];
+                    EndResult = double.Parse(TextBoxScreen.Text);
+                }
+                else if (isNewEntity)
+                {
+                    ChPreviousOperator = ((Button)sender).Text[0];
+                }
+                else
+                {
+                    ChPreviousOperator = ((Button)sender).Text[0];
+                    Operators(EndResult, ChPreviousOperator, double.Parse(TextBoxScreen.Text));
+
+                }
+                isNewEntity = true;
+                isRepeatLastOperation = false;
+            }
+        }
+                        //===========; 
+        private void buttonEqual_Click(object sender, EventArgs e)
+        {
+            if (TextBoxScreen.Text.Length == 0)
+            {
+                TextBoxScreen.Text = "0";
+            }
+            else
+            {
+                if (!isRepeatLastOperation)
+                {
+                    NumbTwo = double.Parse(TextBoxScreen.Text);
+                    isRepeatLastOperation = true;
+                    isNewEntity = true;
+                }
+            
+                Operators(EndResult, ChPreviousOperator, NumbTwo);
+            }
+        }
+ //=======================================================================group3===================================================//
+        // change miuns to positive sign - or +
+        private void buttonChangeSign_Click(object sender, EventArgs e)
+        {
+            if (TextBoxScreen.Text.Length == 0)
+            {
+                TextBoxScreen.Text = "0";
+            }
+            else
+            {
+                TextBoxScreen.Text = (double.Parse(TextBoxScreen.Text) * (-1)).ToString();
+            }
+        }
+
+        // inverse number 1/x or x^-1
+        private void buttonInverse_Click(object sender, EventArgs e)
+        {
+          TextBoxScreen.Text = Math.Pow(double.Parse(TextBoxScreen.Text), -1).ToString();
+        }
+
+        // %  
+        private void buttonPresent_Click(object sender, EventArgs e)
+        {
+            if (TextBoxScreen.Text.Length == 0)
+            {
+                TextBoxScreen.Text = "0";
+            }
+            else
+            {
+                TextBoxScreen.Text = (double.Parse(TextBoxScreen.Text) * 1 / 100).ToString();
+            }
+        }
+
+        //squrt number
+        private void buttonSqurt_Click(object sender, EventArgs e)
+        {
+            if (TextBoxScreen.Text.Length == 0)
+            {
+                TextBoxScreen.Text = "0";
+            }
+            else
+            {
+                TextBoxScreen.Text = Math.Sqrt(double.Parse(TextBoxScreen.Text)).ToString();
+            }
+        }
+//=======================================================================group4===================================================//
+        private void buttonMS_Click(object sender, EventArgs e)
+        {
+            if (TextBoxScreen.Text.Length == 0 || TextBoxScreen.Text=="0")
+            {
+                TextBoxScreen.Text =null;
+            }
+            else
+            {
+                buttonMC.Enabled = true;
+                buttonMR.Enabled = true;
+                Memory = double.Parse(TextBoxScreen.Text);
+            }
+        }
+
+        private void buttonMplus_Click(object sender, EventArgs e)
+        {
+            if (TextBoxScreen.Text.Length == 0 || TextBoxScreen.Text == "0")
+            {
+                TextBoxScreen.Text = null;
+            }
+            else
+            {
+                buttonMC.Enabled = true;
+                buttonMR.Enabled = true;
+                Memory += EndResult;
+                TextBoxScreen.Text = Memory.ToString();
+            }
+        }
+
+        private void buttonMC_Click(object sender, EventArgs e)
+        {
+            Memory = 0;
+            buttonMC.Enabled = false;
+            buttonMR.Enabled = false;
+        }
+
+        private void Calculate_MouseHover(object sender, EventArgs e)
+        {
+            LblDeveloper.Visible = true;
+        }
+
+        private void LblDeveloper_MouseLeave(object sender, EventArgs e)
+        {
+            LblDeveloper.Visible = false;
+        }
+
+        private void buttonMR_Click(object sender, EventArgs e)
+        {
+            TextBoxScreen.Text = Memory.ToString();
+        }
+
+        //=======================================================================group5===================================================//
+
+        //button to delete backspace;
+        private void buttonBackSpace_Click(object sender, EventArgs e)
+        {
+            if (TextBoxScreen.Text.Length == 0)
+            {
+                TextBoxScreen.Text = "0";
+            }
+            else
+            {
+                TextBoxScreen.Text = TextBoxScreen.Text.Remove(TextBoxScreen.Text.Length - 1, 1);
+                isNewEntity = true;
+            }
+        }
+        //button clear text ;
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            TextBoxScreen.Text = "0";
+            NumbTwo = EndResult = 0;
+            isNewEntity = true;
+            ChPreviousOperator = '\0';
+            buttonMC.Enabled = false;
+            buttonMR.Enabled = false;
+        }
+    }
+}
